e1429cc5bbd8a421f1dd82ad2b4b8a6c6addae8c
Ksenia <k.glebova363@mail.com>
2023-01-30 19:19:36 +0400

    tast2

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// // Напишите программу, которая на вход принимает 
+// два числа и выдает, какое число большее, а какое меньшее.
+
+Console.WriteLine ("Введите чило А: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ("Введите чило В: ");
+int numberВ = Convert.ToInt32(Console.ReadLine());
+
+if (numberA>numberВ)
+{
+    Console.WriteLine("Max значение: " + numberA );
+}
+
+else {
+    Console.WriteLine("Max значение: " + numberВ );
+}
