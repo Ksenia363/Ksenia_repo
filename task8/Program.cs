3efb66494edc5ef35f74afc3285d8af7fbfc8288
Ksenia <k.glebova363@mail.com>
2023-01-30 23:40:21 +0400

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine ("Введите чило N: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number>1)
+{
+    for (int i = 2; i <= Number; i = i +2)
+    Console.WriteLine (i + "");
+}
+else
+{
+    Console.WriteLine ("введите положительное четное");
+}
