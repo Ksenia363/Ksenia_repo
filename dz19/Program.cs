bf77624fdb32759ae07c71bec563ab70cb6277d6
Ksenia <k.glebova363@gmail.com>
2023-02-10 16:29:19 +0400

    dz19

--- dz19/Program.cs
+++ dz19/Program.cs
@@ -3,22 +3,37 @@
 // 12821 -> да
 // 23432 -> да
 
-Console.WriteLine("Введите число: ");
-string number = Console.ReadLine();
-int len = number.Length;
+// Console.WriteLine("Введите число: ");
+// string number = Console.ReadLine();
+// int len = number.Length;
 
-if (len == 5)
+// if (len == 5)
+// {
+//     if (number[0] == number[4] && number[1] == number[3])
+//     {
+//         Console.WriteLine("Палиндром");
+//     }
+//     else
+//     {
+//         Console.WriteLine("не палиндром");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("число не является пятизначным, введите пятизначное число");
+// }
+
+System.Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num / 10000 == num % 10 && num / 1000 % 10 == num % 10000 / 1000)
 {
-    if (number[0] == number[4] && number[1] == number[3])
-    {
-        Console.WriteLine("Палиндром");
-    }
-    else
-    {
-        Console.WriteLine("не палиндром");
-    }
+System.Console.WriteLine("Число является палиндромом");
 }
 else
 {
-    Console.WriteLine("число не является пятизначным, введите пятизначное число");
+System.Console.WriteLine("Число не является палиндромом");
 }
+
+// int convertedNum = Convert.ToInt32(Convert.ToString(num[2])); конвертация числа в строку
+// string? num = Console.ReadLine(); 
+// if(num!.Length == 5) восклицательный знак нужен чтобы не обращать внимание на значение Null
