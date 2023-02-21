887bd5795e138318119626112c068448625aa585
Ksenia <k.glebova363@gmail.com>
2023-02-21 14:16:08 +0400

    dz52

--- dz52/Program.cs
+++ dz52/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 52: Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+
+Random random = new Random();
+int[,] myArray = new int[random.Next(1, 10), random.Next(1, 10)];
+for (int i = 0; i < myArray.GetLength(0); i++)
+{
+    for (int j = 0; j < myArray.GetLength(1); j++)
+    {
+        myArray[i, j] = random.Next(1, 10);
+        Console.Write(myArray[i, j] + "\t ");
+    }
+
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int j = 0; j < myArray.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < myArray.GetLength(0); i++)
+    {
+        sum += myArray[i, j];
+    }
+    Console.Write($"{Math.Round( sum / myArray.GetLength(0),2)} \t");
+}
