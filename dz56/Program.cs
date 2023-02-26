28ec01dd96e87c36564b99f7e76f7511d11f592f
Ksenia <k.glebova363@gmail.com>
2023-02-26 14:36:34 +0400

    dz56

--- dz56/Program.cs
+++ dz56/Program.cs
@@ -1,12 +1,10 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
-// В итоге получается вот такой массив:
-// 7 4 2 1
-// 9 5 3 2
-// 8 4 4 2
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
 int ReadInt(string text)
 {
@@ -40,32 +38,35 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
-
-
-void Sortirovka(int[,] matrix)
+void MinSumElements(int[,] matrix)
 {
+    int minznach = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        minznach += matrix[0, i];
+    }
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++) sumRow += matrix[i, j];
+        if (sumRow < minznach)
         {
-            for (int m = 0; m < matrix.GetLength(1) - 1; m++)
-            {
-                if (matrix[i, m] < matrix[i, m + 1])
-                {
-                    int temp = matrix[i, m + 1];
-                    matrix[i, m + 1] = matrix[i, m];
-                    matrix[i, m] = temp;
-                }
-            }
+            minznach = sumRow;
+            minSumRow = i;
         }
+        sumRow = 0;
     }
+    Console.Write($"{minSumRow + 1} строка");
 }
 
 int rows = ReadInt("Введите количество строк матрицы: ");
 int cols = ReadInt("Введите количество столбцов матрицы: ");
 
 var myMatrix = GenerateMatrix(rows, cols);
-PrintMatrix(myMatrix);
-Sortirovka(myMatrix);
+
+MinSumElements(myMatrix);
 Console.WriteLine();
 PrintMatrix(myMatrix);
+
+
