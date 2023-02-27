cc1d0b3679a6b479c2ef898d1520977637772874
Ksenia <k.glebova363@gmail.com>
2023-02-27 09:45:04 +0400

    seminartask65

--- seminartask65/Program.cs
+++ seminartask65/Program.cs
@@ -2,10 +2,17 @@
 // M = 1; N = 5 -> "1, 2, 3, 4, 5"
 // M = 4; N = 8 -> "4, 6, 7, 8"
 
+Metka:
 System.Console.Write("Введите число M: ");
 int m = Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Введите число N: ");
 int n = Convert.ToInt32(Console.ReadLine());
+if (n <= m)
+{
+System.Console.WriteLine("Нет чисел в промежутке от M до N! Повторите ввод!");
+goto Metka;
+}
+
 Print(m, n);
 
 void Print(int numM, int numN)
